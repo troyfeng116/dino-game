bf01d2251fd2fc7341b4af6e0daa5113565f6481
Troy Feng <troy.feng@yale.edu>
2021-01-03 17:10:17 -0500

    Reduce font size of message

--- src/Components/Message/Message.tsx
+++ src/Components/Message/Message.tsx
@@ -12,7 +12,7 @@ export const Message: React.FC<MessageProps> = (props: MessageProps) => {
     if (gameState === GameState.InProgress || messageText.length === 0) return null
 
     return (
-        <div className="z-40 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-3xl uppercase">
+        <div className="z-40 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-2xl uppercase">
             {messageText}
         </div>
     )
